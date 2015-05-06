bf34bc1cc30ead8a4d3e9ebb3db286c2ec25e83d
Sergey Sytchewoj <s.sytchewoj@gmail.com>
2015-05-06 15:36:53 +0300

    map title validation

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,4 +2,6 @@ class Map < ActiveRecord::Base
   has_many :markers, dependent: :destroy
 
   accepts_nested_attributes_for :markers, :reject_if => lambda { |m| m[:lat].blank? }, :allow_destroy => true
+
+  validates_presence_of :title
 end
